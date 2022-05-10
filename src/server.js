f2c0fcf08c3a0244ba1702b694484f90d64ff3d6
Ivan <ibanpillao@gmail.com>
2022-05-10 05:52:55 +0200

    otroMasMas

---
+++ src/server.js
@@ -0,0 +1,14 @@
+// Servidor
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/app-mendimartxak'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/app-mendimartxak/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
